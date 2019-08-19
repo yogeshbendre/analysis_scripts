8adb898ef08498084cfad1d741dc6a4baa6ab483
Yogesh Bendre <ybendre@vmware.com>
2019-08-19 15:32:41 +0530

    add log redirection

--- base_script.sh
+++ base_script.sh
@@ -1,4 +1,4 @@
-python uiBootMonitor.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../delta1.txt -e ../delta2.txt
-python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s lookupsvc
-python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s sts
-python vsanAnalysis.py > vsanout.txt 2> vsanout.txt
+python uiBootMonitor.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../delta1.txt -e ../delta2.txt > /var/log/vmware/uiout_analysis.txt 2> /var/log/vmware/uiout_analysis.txt
+python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s lookupsvc > /var/log/vmware/lookupsvcout_analysis.txt 2> /var/log/vmware/lookupsvcout_analysis.txt
+python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s sts > > /var/log/vmware/stsout_analysis.txt 2> /var/log/vmware/stsout_analysis.txt
+python vsanAnalysis.py > /var/log/vmware/vsanout_analysis.txt 2> /var/log/vmware/vsanout_analysis.txt
