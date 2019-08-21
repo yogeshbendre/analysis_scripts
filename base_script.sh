e013ba9c0d94511ed3adbcdf8ecc41d05857f7fc
Yogesh Bendre <ybendre@vmware.com>
2019-08-21 13:37:19 +0530

    add sps analysis script

--- base_script.sh
+++ base_script.sh
@@ -2,3 +2,4 @@ python uiBootMonitor.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZAT
 python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s lookupsvc > /var/log/vmware/lookupsvcout_analysis.txt 2> /var/log/vmware/lookupsvcout_analysis.txt
 python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s sts > /var/log/vmware/stsout_analysis.txt 2> /var/log/vmware/stsout_analysis.txt
 python vsanAnalysis.py > /var/log/vmware/vsanout_analysis.txt 2> /var/log/vmware/vsanout_analysis.txt
+python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s sps > /var/log/vmware/spsout_analysis.txt 2> /var/log/vmware/spsout_analysis.txt
