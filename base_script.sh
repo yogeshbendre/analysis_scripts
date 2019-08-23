a38278876d7e11435b1c0e4868e5b65d611f5642
Yogesh Bendre <ybendre@vmware.com>
2019-08-23 16:10:02 +0530

    add cls call

--- base_script.sh
+++ base_script.sh
@@ -3,3 +3,4 @@ python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYST
 python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s sts > /var/log/vmware/stsout_analysis.txt 2> /var/log/vmware/stsout_analysis.txt
 python vsanAnalysis.py > /var/log/vmware/vsanout_analysis.txt 2> /var/log/vmware/vsanout_analysis.txt
 python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s sps > /var/log/vmware/spsout_analysis.txt 2> /var/log/vmware/spsout_analysis.txt
+python service_tomcat_component_times.py -f /var/log/vmware/vapi/monitoring/SYSTEM-LEVEL-UTILIZATION/ -d ../ -s content-library > /var/log/vmware/clsout_analysis.txt 2> /var/log/vmware/clsout_analysis.txt
